dae42aeca98614d7ecc292afc23ac5af967e9ae7
“Schrodinger” <realgrishailovan@gmail.com>
2023-04-05 13:16:22 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TestsApp(unittest.TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def test_one(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        pass
---
+++ src/locators/WelcomeLocators.py
@@ -0,0 +1,4 @@
+
+
+class WelcomeLocators:
+    pass
