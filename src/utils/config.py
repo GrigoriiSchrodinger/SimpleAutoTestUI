ff25f09348a9c67ab4386c5d0b7ac42c87091287
Grigorii Schtodinger <schrodinger@MacBook-Pro-Grigorii.local>
2023-04-08 02:39:17 +0300

    Feature: add env for config

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,17 +1,23 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # appium server settings
-APPIUM_HOST = '127.0.0.1'
-APPIUM_PORT = 4723
+APPIUM_HOST = os.environ.get("APPIUM_HOST", "127.0.0.1")
+APPIUM_PORT = os.environ.get("APPIUM_PORT", "4723")
 APPIUM_URL = f'http://{APPIUM_HOST}:{APPIUM_PORT}'
+
+current_dir = os.getcwd()
 
 # device settings
 DESIRED_CAPABILITIES = dict(
-    platformName="Android",
-    platformVersion="13",
-    deviceName="Pixel 6 Pro",
-    automationName="uiautomator2",
-    app="/Users/schrodinger/main/SimpleAutoTestUI/src/TODO.apk",
+    platformName=os.environ.get("OS", "Android"),
+    platformVersion=os.environ.get("VERSION"),
+    deviceName=os.environ.get("NAME_DEVICE"),
+    automationName=os.environ.get("AUTOMATION_NAME", "uiautomator2"),
+    app=os.path.join(current_dir, 'src', 'TODO.apk'),
 )
-
 # appium driver settings
 IMPLICIT_WAIT = 30
 
