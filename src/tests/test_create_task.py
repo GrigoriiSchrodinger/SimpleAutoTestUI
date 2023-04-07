4f5ab94246272c1a6649acefee5253573a9947fb
Grigorii Schtodinger <schrodinger@MacBook-Pro-Grigorii.local>
2023-04-08 01:34:25 +0300

    ref name in test_create_task.py

--- src/tests/test_create_task.py
+++ src/tests/test_create_task.py
@@ -9,21 +9,21 @@ from src.utils.config import DESIRED_CAPABILITIES, APPIUM_URL
 logger = logging.getLogger("root")
 
 
-class TestsCreateTask(unittest.TestCase):
+class TestNewTaskCreation(unittest.TestCase):
     def setUp(self) -> None:
         self.driver = webdriver.Remote(
             command_executor=APPIUM_URL,
             desired_capabilities=DESIRED_CAPABILITIES
         )
 
-    def test_open_new_task_case_01(self) -> None:
+    def test_new_task_page_opens_successfully(self) -> None:
         """Проверяем что страница создания открывается"""
         page = NewTaskPage(self.driver)
         page.new_task_open_page()
 
         self.assertEqual(page.new_task_return_text_title_page(), "New Task")
 
-    def test_create_new_task_case_02(self):
+    def test_new_task_creation_success(self):
         """Проверяем что задача создается"""
         page = NewTaskPage(self.driver)
         page.new_task_open_page()
@@ -34,7 +34,7 @@ class TestsCreateTask(unittest.TestCase):
 
         self.assertEqual(page.list_task_return_text_notice_add_task(), "Task added")
 
-    def test_creation_without_description_case_03(self):
+    def test_new_task_creation_fails_without_description(self):
         """Проверяем что задача не может создаваться без описания"""
         page = NewTaskPage(self.driver)
         page.new_task_open_page()
@@ -44,7 +44,7 @@ class TestsCreateTask(unittest.TestCase):
 
         self.assertEqual(page.new_task_return_text_notice_warning(), "Tasks cannot be empty")
 
-    def test_creation_without_name_case_04(self):
+    def test_new_task_creation_fails_without_name(self):
         """Проверяем что задача не может создаваться без названия"""
         page = NewTaskPage(self.driver)
         page.new_task_open_page()
